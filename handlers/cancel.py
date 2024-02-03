4bbb4d5ab63033935aa9b54c75110ae1790d8265
real_kia <kiavashghaffari@gmail.com>
2024-02-03 17:52:02 +0330

    fixed cancel button

--- handlers/cancel.py
+++ handlers/cancel.py
@@ -2,21 +2,14 @@ from config import dp, bot
 from keyboards.keybaords import startkeyboard
 from aiogram.dispatcher import FSMContext
 from aiogram import types
+
 from handlers.template import new_template
 from handlers.node import node_update, new_node
 from handlers.cnofig_manage import new_config, new_config_template
 def cancel_setup():
-    # @dp.callback_query_handler(text=["cancelask"],state=[new_config,node_update, new_template,new_config_template ])
-    # async def cancel_ask(callback_query: types.CallbackQuery, state: FSMContext):
-    #     await state.finish()
-    #     message_id = callback_query.message.message_id
-    #     chat_id = callback_query.message.chat.id
-    #     await bot.edit_message_text(chat_id=chat_id, message_id=message_id, 
-    #     text='لغو', reply_markup=startkeyboard)
 
 
-    @dp.callback_query_handler(text=["cancelask"])
-    # @dp.callback_query_handler(text=["cancelask"],state=[new_template , node_update, new_node, new_config_template, new_config])
+    @dp.callback_query_handler(text=["cancelask"],state=[new_template , node_update, new_node, new_config_template, new_config])
     async def cancel_ask(callback_query: types.CallbackQuery, state: FSMContext):
         await state.finish()
         message_id = callback_query.message.message_id
